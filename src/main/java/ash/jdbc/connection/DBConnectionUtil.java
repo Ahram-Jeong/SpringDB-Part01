acf0731136531332bc0ffde19922755920628cfb
Ahram-Jeong <ahram1215@gmail.com>
2024-01-28 21:23:27 +0900

    20240128 - 데이터베이스 연결_오탈자 수정

--- src/main/java/ash/jdbc/connection/DBConnectionUtil.java
+++ src/main/java/ash/jdbc/connection/DBConnectionUtil.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static ash.jdbc.connection.ConnenctionConst.*;
+import static ash.jdbc.connection.ConnectionConst.*;
 
 @Slf4j
 public class DBConnectionUtil {
