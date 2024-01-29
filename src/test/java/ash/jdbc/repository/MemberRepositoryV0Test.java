de480c95e55c61b17400c55e5aeecac42c604951
Ahram-Jeong <ahram1215@gmail.com>
2024-01-29 11:29:28 +0900

    20240129 - JDBC 개발_수정, 삭제

--- src/test/java/ash/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/ash/jdbc/repository/MemberRepositoryV0Test.java
@@ -5,9 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -25,5 +25,15 @@ class MemberRepositoryV0Test {
         log.info("member == findMember {}", member == findMember); // 객체 주소 값 비교 -> false
         log.info("member equals findMember {}", member.equals(findMember)); // 객체 내부 값 비교 -> true
         assertThat(findMember).isEqualTo(member);
+
+        // 3. update : money 100000 -> 200000
+        repository.update(member.getMemberId(), 200000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(200000);
+
+        // 4. delete
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(() -> repository.findById(member.getMemberId()))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
